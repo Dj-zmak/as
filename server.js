2ba3a1fcc222caf6275e1e71cfdb2f0728031269
Dj-zmak <arafatx602@outlook.com>
2026-05-04 08:47:10 +0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const pty = require('node-pty');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+
+io.on('connection', (socket) => {
+    const shell = pty.spawn('bash', [], { name: 'xterm-color', cols: 80, rows: 24 });
+    shell.onData(data => socket.emit('output', data));
+    socket.on('input', data => shell.write(data));
+    socket.on('disconnect', () => shell.kill());
+});
+
+server.listen(8080, () => console.log('Terminal running on port 8080'));
