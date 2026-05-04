d2353384955808be08543de1e15ed4c5c60efd80
Dj-zmak <arafatx602@outlook.com>
2026-05-04 08:59:17 +0600

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const pty = require('node-pty');
+const os = require('os');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,10 +11,17 @@ const io = new Server(server);
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 io.on('connection', (socket) => {
-    const shell = pty.spawn('bash', [], { name: 'xterm-color', cols: 80, rows: 24 });
+    const shell = pty.spawn('bash', [], {
+        name: 'xterm-color',
+        cols: 80,
+        rows: 24,
+        cwd: process.env.HOME,
+        env: process.env
+    });
+
     shell.onData(data => socket.emit('output', data));
     socket.on('input', data => shell.write(data));
     socket.on('disconnect', () => shell.kill());
 });
 
-server.listen(8080, () => console.log('Terminal running on port 8080'));
+server.listen(8080, '0.0.0.0', () => console.log('Terminal running on port 8080'));
